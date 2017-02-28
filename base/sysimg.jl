688b9cab6967e504d4b0abddb1198a02440021cb
Curtis Vogt <curtis.vogt@gmail.com>
2017-02-27 20:20:21 -0600

    readuntil with on-the-fly backtrack caching
    Caches backtracking information as it is needed. Using a SparseVector
    which has a lower memory footprint than Vector but is more performant
    than Dict.

--- base/sysimg.jl
+++ base/sysimg.jl
@@ -356,6 +356,7 @@ import .Dates: Date, DateTime, DateFormat, @dateformat_str, now
 include("sparse/sparse.jl")
 importall .SparseArrays
 
+include("readuntil.jl")
 include("asyncmap.jl")
 
 include("distributed/Distributed.jl")
