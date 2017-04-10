934bebc2a8b80ea233354ce4e54a1122269ddada
Jeff Bezanson <jeff.bezanson@gmail.com>
2017-04-10 17:08:19 -0400

    always use libexec for embedding executable

--- contrib/windows/msys_build.sh
+++ contrib/windows/msys_build.sh
@@ -206,4 +206,4 @@ cat Make.user
 make -j3 VERBOSE=1
 make build-stats
 #make debug
-make prefix=/c/projects/julia/usr VERBOSE=1 -C examples
+make VERBOSE=1 -C examples
--- test/embedding.jl
+++ test/embedding.jl
@@ -1,9 +1,9 @@
 # test that the embedding example runs without error
 let
     if is_windows()
-        embedding_bin = joinpath(JULIA_HOME,"embedding.exe")
+        embedding_bin = joinpath(JULIA_HOME,Base.LIBEXECDIR,"embedding.exe")
     else
-        embedding_bin = joinpath(JULIA_HOME,"..","libexec","embedding")
+        embedding_bin = joinpath(JULIA_HOME,Base.LIBEXECDIR,"embedding")
     end
     lines = readlines(pipeline(`$(embedding_bin)`,
                                stderr=DevNull))
